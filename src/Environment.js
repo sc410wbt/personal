4bb028c55af7647232ccc9f5d3da97dcae5041ea
Sun Chen <chen.shenglun@gmail.com>
2021-08-05 20:50:05 -0700

    functioning separate loader file

--- src/Environment.js
+++ src/Environment.js
@@ -6,6 +6,8 @@ import {OBJLoader} from "three/examples/jsm/loaders/OBJLoader"
 import {MTLLoader} from "three/examples/jsm/loaders/MTLLoader"
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 
+import Loader from "./library/Loader"
+
 import spriteImg from './images/sprite.png'
 
 import s from './Environment.module.sass'
@@ -98,39 +100,10 @@ export default function Environment() {
 		// })
 
 
-
-
-		let loader = new GLTFLoader()
-
-		loader.load(
-			'/models/ring/scene.gltf',
-			(object) => {
-				console.log('success')
-				let scale = 1
-				// object.scene.position.set(11, 0.2, 23)
-				object.scene.scale.set(scale, scale, scale)
-				object.scene.traverse( function( child ) {
-					if ( child.isMesh ) {
-						// console.log('child', child.geometry)
-						child.castShadow = true
-						child.receiveShadow = true
-						child.material.metalness = 0.5
-						// child.material.roughness = 0.5
-						placeSprites(child.geometry.attributes.position.array)
-					}
-				} )
-				object.scene.rotation.set(0, Math.PI / 3, 0)
-				object.scene.position.set(10, 0, 0)
-				// scene.add(object.scene)
-			},
-			() => {
-
-			},
-			(error) => {
-				console.log(error)
-			}
-		)
-
+		let loader = new Loader('/models/ring/scene.gltf')
+		let object = await loader.load()
+		console.log(object)
+		scene.add(object)
 
 	}
 
@@ -195,122 +168,9 @@ export default function Environment() {
 		}
 	}
 
-	async function placeSprites(position) {
-		// console.log(position)
-		let threshhold = 0.1
-		let upperThreshold = 1.5
-		let multiplier = 3
-		let group = new THREE.Group()
-		group.rotation.set(0 - Math.PI / 2, 0, 0)
-
-		let spriteScale = 0.05
-		const map = await new THREE.TextureLoader().load( '/images/sprite.png' )
-		// console.log('sprite map', map)
-		const material = new THREE.SpriteMaterial({ map: map })
-		const tempMaterial = new THREE.SpriteMaterial({ map: map })
-
-
-		let lastX
-		let lastY
-		let lastZ
-
-
-		// const lineMaterial = new THREE.LineBasicMaterial({
-		// 	color: 0x69ffff,
-		// 	transparent: true,
-		// 	opacity: 0.2
-		// })
 
 
 
-		for (let i = 0; i < position.length; i += 6) {
-			let x = position[i] * multiplier
-			let y = position[i + 1] * multiplier
-			let z = position[i + 2] * multiplier
-
-			// Space them out
-			if (lastX) {
-				// console.log('compare', lastX, x)
-				if (Math.abs(x - lastX) < threshhold * multiplier &&
-					Math.abs(y - lastY) < threshhold * multiplier &&
-					Math.abs(z - lastZ) < threshhold * multiplier) {
-					// not enough distance, skip
-					continue
-				}
-			}
-
-
-
-
-			const sprite = new THREE.Sprite(material)
-			let [rX, rY, rZ] = randomizePoints(x, y, z)
-			sprite.position.set(rX, rY, rZ)
-			let randomScale = spriteScale + Math.random() * 0.1
-			sprite.scale.set(randomScale, randomScale, randomScale)
-			// console.log(sprite)
-			group.add(sprite)
-
-			if (lastX) {
-				// Keep this if statement in, might be more efficient before getting to distance calc
-				if (Math.abs(x - lastX) > upperThreshold * multiplier ||
-					Math.abs(y - lastY) > upperThreshold * multiplier ||
-					Math.abs(z - lastZ) > upperThreshold * multiplier) {
-					// too far, add stuff in between
-
-					let distance = Math.sqrt(Math.pow(x - lastX, 2) + Math.pow(y - lastY, 2) + Math.pow(z - lastZ, 2))
-					let iterations = Math.ceil(distance)
-
-					for (let i = 1; i < iterations; i++) {
-						let newX = lastX + ((x - lastX) / iterations) * i
-						let newY = lastY + ((y - lastY) / iterations) * i
-						let newZ = lastZ + ((z - lastZ) / iterations) * i
-						const sprite = new THREE.Sprite(tempMaterial)
-						let [rnX, rnY, rnZ] = randomizePoints(newX, newY, newZ, 0.5)
-						sprite.position.set(rnX, rnY, rnZ)
-						// sprite.position.set(x + 0.1, y + 0.1, z + 0.1)
-						let randomScale = spriteScale + Math.random() * 0.2
-						sprite.scale.set(randomScale, randomScale, randomScale)
-						group.add(sprite)
-						// console.log('adding', x, lastX, y, lastY, z, lastZ)
-					}
-
-
-
-
-
-					// const points = []
-					// points.push( new THREE.Vector3( x, y, z ) )
-					// points.push( new THREE.Vector3( lastX, lastY, lastZ ) )
-					//
-					// const geometry = new THREE.BufferGeometry().setFromPoints( points )
-					//
-					// const line = new THREE.Line( geometry, lineMaterial )
-					// group.add( line )
-
-				}
-			}
-
-			// console.log('sprite placement', x,y,x)
-			lastX = x
-			lastY = y
-			lastZ = z
-
-			// let cubeGeometry = new THREE.BoxBufferGeometry(0.05, 0.05, 0.05)
-			// let cubeMaterial = new THREE.MeshPhysicalMaterial({ color: 0x888888 })
-			//
-			// let cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-			// cube.position.set(x, y, z)
-			// scene.add(cube)
-		}
-		scene.add(group)
-	}
-
-	function randomizePoints(x, y, z, intensity = 0.1) {
-		x = x - intensity + Math.random() * intensity * 2
-		y = y - intensity + Math.random() * intensity * 2
-		z = z - intensity + Math.random() * intensity * 2
-		return [x, y, z]
-	}
 
 	function light() {
 		// let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
---
+++ src/library/Loader.js
@@ -0,0 +1,153 @@
+import * as THREE from 'three'
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
+
+export default function Loader(url) {
+
+	this.url = url
+	this.load = async function() {
+
+
+		let loader = new GLTFLoader()
+		let object = await loader.loadAsync(url)
+
+		let scale = 1
+		// object.scene.position.set(11, 0.2, 23)
+		object.scene.scale.set(scale, scale, scale)
+		let meshGroup = new THREE.Group()
+		object.scene.traverse(function (child) {
+			if (child.isMesh) {
+				// console.log('child', child.geometry)
+				child.castShadow = true
+				child.receiveShadow = true
+				child.material.metalness = 0.5
+				// child.material.roughness = 0.5
+				meshGroup.add(placeSprites(child.geometry.attributes.position.array))
+			}
+		})
+		object.scene.rotation.set(0, Math.PI / 3, 0)
+		object.scene.position.set(10, 0, 0)
+		// scene.add(object.scene)
+		return meshGroup
+
+
+	}
+
+}
+
+function placeSprites(position) {
+	// console.log(position)
+	let threshhold = 0.1
+	let upperThreshold = 1.5
+	let multiplier = 3
+	let group = new THREE.Group()
+	group.rotation.set(0 - Math.PI / 2, 0, 0)
+
+	let spriteScale = 0.05
+	const map = new THREE.TextureLoader().load( '/images/sprite.png' )
+	// console.log('sprite map', map)
+	const material = new THREE.SpriteMaterial({ map: map })
+	const tempMaterial = new THREE.SpriteMaterial({ map: map })
+
+
+	let lastX
+	let lastY
+	let lastZ
+
+
+	// const lineMaterial = new THREE.LineBasicMaterial({
+	// 	color: 0x69ffff,
+	// 	transparent: true,
+	// 	opacity: 0.2
+	// })
+
+
+
+	for (let i = 0; i < position.length; i += 6) {
+		let x = position[i] * multiplier
+		let y = position[i + 1] * multiplier
+		let z = position[i + 2] * multiplier
+
+		// Space them out
+		if (lastX) {
+			// console.log('compare', lastX, x)
+			if (Math.abs(x - lastX) < threshhold * multiplier &&
+				Math.abs(y - lastY) < threshhold * multiplier &&
+				Math.abs(z - lastZ) < threshhold * multiplier) {
+				// not enough distance, skip
+				continue
+			}
+		}
+
+
+
+
+		const sprite = new THREE.Sprite(material)
+		let [rX, rY, rZ] = randomizePoints(x, y, z)
+		sprite.position.set(rX, rY, rZ)
+		let randomScale = spriteScale + Math.random() * 0.1
+		sprite.scale.set(randomScale, randomScale, randomScale)
+		// console.log(sprite)
+		group.add(sprite)
+
+		if (lastX) {
+			// Keep this if statement in, might be more efficient before getting to distance calc
+			if (Math.abs(x - lastX) > upperThreshold * multiplier ||
+				Math.abs(y - lastY) > upperThreshold * multiplier ||
+				Math.abs(z - lastZ) > upperThreshold * multiplier) {
+				// too far, add stuff in between
+
+				let distance = Math.sqrt(Math.pow(x - lastX, 2) + Math.pow(y - lastY, 2) + Math.pow(z - lastZ, 2))
+				let iterations = Math.ceil(distance)
+
+				for (let i = 1; i < iterations; i++) {
+					let newX = lastX + ((x - lastX) / iterations) * i
+					let newY = lastY + ((y - lastY) / iterations) * i
+					let newZ = lastZ + ((z - lastZ) / iterations) * i
+					const sprite = new THREE.Sprite(tempMaterial)
+					let [rnX, rnY, rnZ] = randomizePoints(newX, newY, newZ, 0.5)
+					sprite.position.set(rnX, rnY, rnZ)
+					// sprite.position.set(x + 0.1, y + 0.1, z + 0.1)
+					let randomScale = spriteScale + Math.random() * 0.2
+					sprite.scale.set(randomScale, randomScale, randomScale)
+					group.add(sprite)
+					// console.log('adding', x, lastX, y, lastY, z, lastZ)
+				}
+
+
+
+
+
+				// const points = []
+				// points.push( new THREE.Vector3( x, y, z ) )
+				// points.push( new THREE.Vector3( lastX, lastY, lastZ ) )
+				//
+				// const geometry = new THREE.BufferGeometry().setFromPoints( points )
+				//
+				// const line = new THREE.Line( geometry, lineMaterial )
+				// group.add( line )
+
+			}
+		}
+
+		// console.log('sprite placement', x,y,x)
+		lastX = x
+		lastY = y
+		lastZ = z
+
+		// let cubeGeometry = new THREE.BoxBufferGeometry(0.05, 0.05, 0.05)
+		// let cubeMaterial = new THREE.MeshPhysicalMaterial({ color: 0x888888 })
+		//
+		// let cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+		// cube.position.set(x, y, z)
+		// scene.add(cube)
+	}
+	return group
+}
+
+
+function randomizePoints(x, y, z, intensity = 0.1) {
+	x = x - intensity + Math.random() * intensity * 2
+	y = y - intensity + Math.random() * intensity * 2
+	z = z - intensity + Math.random() * intensity * 2
+	return [x, y, z]
+}
