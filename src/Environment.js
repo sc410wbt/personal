247c0d3e1296cbac11aba3790c1dafe4a91bfcd8
Sun Chen <chen.shenglun@gmail.com>
2021-08-05 23:21:05 -0700

    add bell curved randomization

--- src/Environment.js
+++ src/Environment.js
@@ -246,8 +246,16 @@ export default function Environment() {
 }
 
 
+function randn_bm() {
+	let u = 0, v = 0;
+	while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+	while(v === 0) v = Math.random();
+	return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
+}
+
 function randomizeSpriteScale() {
 	let baseScale = 0.02
-	let randomScale = baseScale + 0.12 * Math.random()
+	let randomScale = baseScale + 0.12 * randn_bm()
+	if (randomScale > 0.12) console.log('final scale', randomScale)
 	return [randomScale, randomScale, randomScale]
 }
