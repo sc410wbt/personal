d51937d92bd514ad37c56c5af18177a00371d91e
Sun Chen <chen.shenglun@gmail.com>
2021-08-05 21:18:49 -0700

    add nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from "./components/Header"
+import Nav from './components/Nav'
 import Environment from "./Environment"
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Nav />
             <Environment />
         </div>
     )
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import s from './Nav.module.sass'
+
+export default function Nav() {
+
+	return (
+		<div className={s.wrapper}>
+			<div>Home</div>
+			<div>Inspiration Museum</div>
+			<div>Augmented Reality</div>
+		</div>
+	)
+
+}
