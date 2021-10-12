42faec4da27d809bfe6a8785d34356bae515b6cc
Sun Chen <chen.shenglun@gmail.com>
2021-10-11 17:11:57 -0700

    add basic intro animation and intersection observer

--- src/sections/templates/Intro.js
+++ src/sections/templates/Intro.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import cx from 'classnames'
+import {useInView} from "react-intersection-observer"
 
 import s from './Intro.module.sass'
 
 export default function IntroTemplate({ text }) {
 
+	const {ref, inView} = useInView()
+
+	useEffect(() => {
+		if (inView) {
+
+		}
+	}, [inView])
+
 	return (
 		<div className={s.wrapper}>
-			<div className={s.panel}>
+			<div className={cx([s.panel, { [s.active]: inView }])} ref={ref}>
 				{text}
 			</div>
 		</div>
