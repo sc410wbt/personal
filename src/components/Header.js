cd41f1224ea1e223714be660bc303cf92aab4f99
Sun Chen <chen.shenglun@gmail.com>
2021-10-16 13:58:47 -0700

    header close button (return to home)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import {useLocation} from "react-router"
+import {useLocation, useHistory} from "react-router"
 import cx from 'classnames'
 
 
@@ -7,13 +7,18 @@ import s from './Header.module.sass'
 
 export default function Header() {
 
+	const history = useHistory()
 	const location = useLocation()
+
+	function handleClick() {
+		history.push('/')
+	}
 
 	return (
 		<div className={s.wrapper}>
 			<header>
 				<strong>WORK</strong> by Sun Chen
-				<div className={cx(s.close, { [s.active]: location.pathname !== '/' })}>
+				<div className={cx(s.close, { [s.active]: location.pathname !== '/' })} onClick={handleClick}>
 					<span />
 					<span />
 				</div>
