2e3e37feebaeac471574e8d23e86e128f31b525a
Sun Chen <chen.shenglun@gmail.com>
2021-10-15 19:30:58 -0700

    overscroll on each page

--- src/pages/projects/InspirationMuseum.js
+++ src/pages/projects/InspirationMuseum.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import TitleTemplate from "../../sections/templates/Title"
+import IntroTemplate from "../../sections/templates/Intro";
 
 export default function InspirationMuseumPage() {
 
 	return (
 		<div className={"page"}>
 			<TitleTemplate title={"Inspiration Museum"} />
+			<IntroTemplate />
 		</div>
 	)
 
