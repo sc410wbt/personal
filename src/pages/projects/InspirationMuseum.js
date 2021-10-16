d77353237852b9d89112ade64f226249df188a30
Sun Chen <chen.shenglun@gmail.com>
2021-10-16 14:33:58 -0700

    wrap inspiration museum page

--- src/pages/projects/InspirationMuseum.js
+++ src/pages/projects/InspirationMuseum.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import TitleTemplate from "../../sections/templates/Title"
-import IntroTemplate from "../../sections/templates/Intro";
+import IntroTemplate from "../../sections/templates/Intro"
+import ScrollProgress from "../../components/ScrollProgress"
 
 export default function InspirationMuseumPage() {
 
 	return (
 		<div className={"page"}>
-			<TitleTemplate title={"Inspiration Museum"} object={"rhino"}/>
-			<IntroTemplate />
+			<div style={{ position: 'relative' }}>
+				<ScrollProgress />
+				<TitleTemplate title={"Inspiration Museum"} object={"rhino"}/>
+				<IntroTemplate />
+			</div>
 		</div>
 	)
 
