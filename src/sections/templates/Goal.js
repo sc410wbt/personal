632e65c6636a2f6bb6807e70615e64bef6ffe844
Sun Chen <chen.shenglun@gmail.com>
2021-10-16 16:30:35 -0700

    goal simple animations and dynamic margin

--- src/sections/templates/Goal.js
+++ src/sections/templates/Goal.js
@@ -1,17 +1,24 @@
 import React, {useEffect, useState} from 'react'
+import {useInView} from "react-intersection-observer"
+import cx from 'classnames'
 
 import s from './Goal.module.sass'
 
 export default function Goal({ goal, no }) {
 
 	const [margin, setMargin] = useState(0)
+	const [ref, inView] = useInView()
 
 	useEffect(() => {
 		setMargin(Math.random() * 70)
 	}, [])
 
+	useEffect(() => {
+
+	}, [inView])
+
 	return (
-		<div className={s.goal} style={{ marginLeft: `${margin}%` }}>
+		<div className={cx(s.goal, { [s.active]: inView })} style={{ marginLeft: `${margin}%` }} ref={ref}>
 			<div className={s.number}>{no + 1}</div>
 			{goal}
 		</div>
