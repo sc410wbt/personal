07f3154f6c9023701a439e5c7eba50d2f624f890
Sun Chen <chen.shenglun@gmail.com>
2021-10-16 14:43:29 -0700

    add end tab to scroll ticks

--- src/components/ScrollProgress.js
+++ src/components/ScrollProgress.js
@@ -6,6 +6,9 @@ export default function scrollProgress() {
 	return (
 		<div className={s.wrapper}>
 			<div className={s.fade} />
+			<div className={s.end}>
+			<div className={s.text}>END</div>
+			</div>
 		</div>
 	)
 }
