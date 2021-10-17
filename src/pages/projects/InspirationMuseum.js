1466f8bad7f479a93d944b941ad2ee8d6b93c87c
Sun Chen <chen.shenglun@gmail.com>
2021-10-16 18:52:50 -0700

    add goals to inspiration museum

--- src/pages/projects/InspirationMuseum.js
+++ src/pages/projects/InspirationMuseum.js
@@ -5,12 +5,18 @@ import ScrollProgress from "../../components/ScrollProgress"
 
 export default function InspirationMuseumPage() {
 
+	const goals = [
+		'Engage VIP in retail stores',
+		'Browse the rich catalog of Cartier designs',
+		'Upsell them on a custom design'
+	]
+
 	return (
 		<div className={"page"}>
 			<div style={{ position: 'relative' }}>
 				<ScrollProgress />
 				<TitleTemplate title={"Inspiration Museum"} object={"rhino"}/>
-				<IntroTemplate />
+				<IntroTemplate goals={goals} />
 			</div>
 		</div>
 	)
